fb167c0892d7ca82252b27f92565f3ee126e184f
Arash <arashkarimpourg@gmail.com>
2023-07-14 18:38:25 +0800

    Use PNG files for maximize, minimize and close buttons.

--- src/main/java/edu/ucsi/bookkeeper/BookKeeper.java
+++ src/main/java/edu/ucsi/bookkeeper/BookKeeper.java
@@ -1,15 +1,17 @@
 package edu.ucsi.bookkeeper;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.io.InputStream;
 import java.net.URL;
 
 public class BookKeeper extends Application {
@@ -33,20 +35,56 @@ public class BookKeeper extends Application {
         // Title Bar
         HBox titleBar = new HBox();
         titleBar.getStyleClass().add("title-bar");
-        titleBar.setPadding(new Insets(10));
-        titleBar.setAlignment(Pos.CENTER_RIGHT);
+        titleBar.setAlignment(Pos.TOP_RIGHT);
 
-        Button closeButton = new Button("X");
+        Button closeButton = new Button();
+        closeButton.setId("close-button");
         closeButton.getStyleClass().add("window-button");
         closeButton.setOnAction(event -> primaryStage.close());
 
-        Button maximizeButton = new Button("[]");
+        // Imports the PNG file while avoiding NullPointerException
+        InputStream imageStreamClose = getClass().getResourceAsStream("images/title-bar/close.png");
+        if (imageStreamClose != null) {
+            Image closeImage = new Image(imageStreamClose);
+            ImageView closeIcon = new ImageView(closeImage);
+            closeIcon.setFitWidth(14);
+            closeIcon.setFitHeight(14);
+            closeButton.setGraphic(closeIcon);
+        } else {
+            System.err.println("Unable to load close.png");
+        }
+
+        Button maximizeButton = new Button();
         maximizeButton.getStyleClass().add("window-button");
         maximizeButton.setOnAction(event -> primaryStage.setMaximized(!primaryStage.isMaximized()));
 
-        Button minimizeButton = new Button("_");
+        // Imports the PNG file while avoiding NullPointerException
+        InputStream imageStreamMaximize = getClass().getResourceAsStream("images/title-bar/maximize.png");
+        if (imageStreamMaximize != null) {
+            Image maximizeImage = new Image(imageStreamMaximize);
+            ImageView maximizeIcon = new ImageView(maximizeImage);
+            maximizeIcon.setFitWidth(14);
+            maximizeIcon.setFitHeight(14);
+            maximizeButton.setGraphic(maximizeIcon);
+        } else {
+            System.err.println("Unable to load maximize.png");
+        }
+
+        Button minimizeButton = new Button();
         minimizeButton.getStyleClass().add("window-button");
         minimizeButton.setOnAction(event -> primaryStage.setIconified(true));
+
+        // Imports the PNG file while avoiding NullPointerException
+        InputStream imageStreamMinimize = getClass().getResourceAsStream("images/title-bar/minimize.png");
+        if (imageStreamMinimize != null) {
+            Image minimizeImage = new Image(imageStreamMinimize);
+            ImageView minimizeIcon = new ImageView(minimizeImage);
+            minimizeIcon.setFitWidth(14);
+            minimizeIcon.setFitHeight(14);
+            minimizeButton.setGraphic(minimizeIcon);
+        } else {
+            System.err.println("Unable to load minimize.png");
+        }
 
         titleBar.getChildren().addAll(minimizeButton, maximizeButton, closeButton);
 
