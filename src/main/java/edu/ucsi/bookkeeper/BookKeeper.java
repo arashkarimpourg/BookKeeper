c927b6869cb05c1216bff5160c55b3d878a9c45d
Arash <arashkarimpourg@gmail.com>
2023-07-15 14:23:44 +0800

    Cleaned up the code.

--- src/main/java/edu/ucsi/bookkeeper/BookKeeper.java
+++ src/main/java/edu/ucsi/bookkeeper/BookKeeper.java
@@ -1,15 +1,18 @@
 package edu.ucsi.bookkeeper;
 
 import javafx.application.Application;
+import javafx.animation.TranslateTransition;
+import javafx.scene.layout.*;
+import javafx.util.Duration;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.scene.paint.Color;
 
 import java.io.InputStream;
 import java.net.URL;
@@ -18,75 +21,117 @@ public class BookKeeper extends Application {
 
     private double xOffset = 0;
     private double yOffset = 0;
+    private boolean isUserEditPaneVisible = false;
 
     @Override
     public void start(Stage primaryStage) {
         primaryStage.initStyle(StageStyle.TRANSPARENT);
 
-        // Left Sidebar
-        VBox leftSidebar = new VBox();
-        leftSidebar.getStyleClass().add("left-sidebar");
-        leftSidebar.setPrefWidth(200);
-        leftSidebar.setFillWidth(true);
-        leftSidebar.setAlignment(Pos.CENTER);
-        leftSidebar.setSpacing(4);
+        // Create main window
+        BorderPane root = new BorderPane();
+        root.getStyleClass().add("round-corners");
 
-        Button usersTab = new Button("Users");
-        usersTab.getStyleClass().add("sidebar-button");
-        usersTab.setOnAction(event -> {
-            // Logic to change content pane for Button 1
-        });
-        usersTab.setMaxWidth(Double.MAX_VALUE);
+        // Create left pane
+        VBox leftPane = new VBox();
+        leftPane.setPrefWidth(200);
+        leftPane.setId("left-pane");
 
-        // Imports the PNG file while avoiding NullPointerException
-        InputStream imageStreamUsers = getClass().getResourceAsStream("images/side-bar/users.png");
-        if (imageStreamUsers != null) {
-            Image usersImage = new Image(imageStreamUsers);
-            ImageView usersIcon = new ImageView(usersImage);
-            usersIcon.setFitWidth(26);
-            usersIcon.setFitHeight(26);
-            usersTab.setGraphic(usersIcon);
-        } else {
-            System.err.println("Unable to load users.png");
-        }
-
-        Button booksTab = new Button("Books");
-        booksTab.getStyleClass().add("sidebar-button");
-        booksTab.setOnAction(event -> {
-            // Logic to change content pane for Button 2
-        });
-        booksTab.setMaxWidth(Double.MAX_VALUE);
-
-        // Imports the PNG file while avoiding NullPointerException
-        InputStream imageStreamBooks = getClass().getResourceAsStream("images/side-bar/books.png");
-        if (imageStreamBooks != null) {
-            Image booksImage = new Image(imageStreamBooks);
-            ImageView booksIcon = new ImageView(booksImage);
-            booksIcon.setFitWidth(26);
-            booksIcon.setFitHeight(26);
-            booksTab.setGraphic(booksIcon);
-        } else {
-            System.err.println("Unable to load books.png");
-        }
-
-        leftSidebar.getChildren().addAll(usersTab, booksTab);
-
-        // Right Pane
-        VBox rightPane = new VBox();
-        rightPane.getStyleClass().add("right-pane");
-
-        // Title Bar
+        // Create title bar
         HBox titleBar = new HBox();
-        titleBar.getStyleClass().add("title-bar");
-        titleBar.setAlignment(Pos.TOP_RIGHT);
+        titleBar.setId("title-bar");
 
+        // Create back button
+        Button backButton = new Button();
+        backButton.setId("back-button");
+
+        // Null check for back button icon
+        InputStream imageStreamBack = getClass().getResourceAsStream("images/title-bar/back.png");
+        if (imageStreamBack != null) {
+            Image backImage = new Image(imageStreamBack);
+            ImageView backIcon = new ImageView(backImage);
+            backIcon.setFitWidth(17);
+            backIcon.setFitHeight(17);
+            backButton.setGraphic(backIcon);
+        } else {
+            System.err.println("Unable to load back.png");
+        }
+
+        // Create title
+        Label title = new Label("Book Keeper");
+        title.setId("title");
+
+        // Add back button and title to title bar
+        titleBar.getChildren().addAll(backButton, title);
+
+        // Create sidebar
+        VBox sidebar = new VBox();
+        sidebar.setAlignment(Pos.CENTER);
+        sidebar.setSpacing(4);
+        sidebar.setId("sidebar");
+
+        // Create user tab
+        Button userTab = new Button("Users");
+        userTab.getStyleClass().add("sidebar-button");
+        userTab.setMaxWidth(Double.MAX_VALUE);
+        userTab.setOnAction(event -> {
+            // Logic to change userContentPane for userTab
+        });
+
+        // Null check for user tab icon
+        InputStream imageStreamUser = getClass().getResourceAsStream("images/sidebar/user.png");
+        if (imageStreamUser != null) {
+            Image userImage = new Image(imageStreamUser);
+            ImageView userIcon = new ImageView(userImage);
+            userIcon.setFitWidth(26);
+            userIcon.setFitHeight(26);
+            userTab.setGraphic(userIcon);
+        } else {
+            System.err.println("Unable to load user.png");
+        }
+
+        // Create book tab
+        Button bookTab = new Button("Books");
+        bookTab.getStyleClass().add("sidebar-button");
+        bookTab.setMaxWidth(Double.MAX_VALUE);
+        bookTab.setOnAction(event -> {
+            // Logic to change userContentPane for bookTab
+        });
+
+        // Null check for book tab icon
+        InputStream imageStreamBook = getClass().getResourceAsStream("images/sidebar/book.png");
+        if (imageStreamBook != null) {
+            Image bookImage = new Image(imageStreamBook);
+            ImageView bookIcon = new ImageView(bookImage);
+            bookIcon.setFitWidth(26);
+            bookIcon.setFitHeight(26);
+            bookTab.setGraphic(bookIcon);
+        } else {
+            System.err.println("Unable to load book.png");
+        }
+
+        // Add user & book tabs to sidebar
+        sidebar.getChildren().addAll(userTab, bookTab);
+
+        // Add title bar & sidebar to left pane
+        leftPane.getChildren().addAll(titleBar, sidebar);
+
+        // Create right pane
+        VBox rightPane = new VBox();
+        rightPane.setId("right-pane");
+
+        // Create window controls pane
+        HBox windowControlsPane = new HBox();
+        windowControlsPane.setId("window-controls-pane");
+        windowControlsPane.setAlignment(Pos.TOP_RIGHT);
+
+        // Create close window button
         Button closeButton = new Button();
         closeButton.setId("close-button");
-        closeButton.getStyleClass().add("window-button");
+        closeButton.getStyleClass().add("window-controls-button");
         closeButton.setOnAction(event -> primaryStage.close());
 
-        // Imports the PNG file while avoiding NullPointerException
-        InputStream imageStreamClose = getClass().getResourceAsStream("images/title-bar/close.png");
+        // Null check for close window icon
+        InputStream imageStreamClose = getClass().getResourceAsStream("images/window-controls-pane/close.png");
         if (imageStreamClose != null) {
             Image closeImage = new Image(imageStreamClose);
             ImageView closeIcon = new ImageView(closeImage);
@@ -97,12 +142,13 @@ public class BookKeeper extends Application {
             System.err.println("Unable to load close.png");
         }
 
+        // Create maximize window button
         Button maximizeButton = new Button();
-        maximizeButton.getStyleClass().add("window-button");
+        maximizeButton.getStyleClass().add("window-controls-button");
         maximizeButton.setOnAction(event -> primaryStage.setMaximized(!primaryStage.isMaximized()));
 
-        // Imports the PNG file while avoiding NullPointerException
-        InputStream imageStreamMaximize = getClass().getResourceAsStream("images/title-bar/maximize.png");
+        // Null check for maximize window icon
+        InputStream imageStreamMaximize = getClass().getResourceAsStream("images/window-controls-pane/maximize.png");
         if (imageStreamMaximize != null) {
             Image maximizeImage = new Image(imageStreamMaximize);
             ImageView maximizeIcon = new ImageView(maximizeImage);
@@ -113,12 +159,13 @@ public class BookKeeper extends Application {
             System.err.println("Unable to load maximize.png");
         }
 
+        // Create minimize window button
         Button minimizeButton = new Button();
-        minimizeButton.getStyleClass().add("window-button");
+        minimizeButton.getStyleClass().add("window-controls-button");
         minimizeButton.setOnAction(event -> primaryStage.setIconified(true));
 
-        // Imports the PNG file while avoiding NullPointerException
-        InputStream imageStreamMinimize = getClass().getResourceAsStream("images/title-bar/minimize.png");
+        // Null check for minimize window icon
+        InputStream imageStreamMinimize = getClass().getResourceAsStream("images/window-controls-pane/minimize.png");
         if (imageStreamMinimize != null) {
             Image minimizeImage = new Image(imageStreamMinimize);
             ImageView minimizeIcon = new ImageView(minimizeImage);
@@ -129,24 +176,59 @@ public class BookKeeper extends Application {
             System.err.println("Unable to load minimize.png");
         }
 
-        titleBar.getChildren().addAll(minimizeButton, maximizeButton, closeButton);
+        // Add buttons to window controls pane
+        windowControlsPane.getChildren().addAll(minimizeButton, maximizeButton, closeButton);
 
-        // Content Pane
-        Pane contentPane = new Pane();
-        contentPane.getStyleClass().add("content-pane");
+        // Create main pane
+        Pane mainPane = new Pane();
+        mainPane.setId("main-pane");
 
-        rightPane.getChildren().addAll(titleBar, contentPane);
+        // Create user pane
+        StackPane userPane = new StackPane();
+        userPane.setId("user-pane");
 
-        // Main Layout
-        BorderPane root = new BorderPane();
-        root.getStyleClass().add("rounded-corner-pane");
-        root.setLeft(leftSidebar);
+        // Create user content pane
+        VBox userContentPane = new VBox();
+        userContentPane.setId("user-content-pane");
+
+        // Create user content pane title
+        Label userContentPaneTitle = new Label("Users");
+        userContentPaneTitle.setId("user-content-pane-title");
+
+        // Create user edit button
+        Button userEditButton = new Button("EDIT USER");
+        userEditButton.setOnAction(e -> toggleUserEditPane(userPane));
+
+        // Add user content pane and user edit title to user content pane
+        userContentPane.getChildren().addAll(userContentPaneTitle, userEditButton);
+
+        // Create user edit pane
+        VBox userEditPane = new VBox();
+        userEditPane.setId("user-edit-pane");
+        userEditPane.setTranslateX(userContentPane.getWidth());
+        userEditPane.setVisible(false);
+
+        // Add user content pane and user edit pane to user pane
+        userPane.getChildren().addAll(userContentPane, userEditPane);
+
+        // Set action for back button
+        backButton.setOnAction(e -> toggleUserEditPane(userPane));
+
+        // Add user pane to main pane
+        mainPane.getChildren().addAll(userPane);
+
+        // Add window controls pane and main pane to right pane
+        rightPane.getChildren().addAll(windowControlsPane, mainPane);
+
+        // Add left pane & right pane to main window
+        root.setLeft(leftPane);
         root.setCenter(rightPane);
 
+        // Create scene
         Scene scene = new Scene(root, 800, 600);
         scene.setFill(Color.TRANSPARENT);
 
-        // Imports the CSS file while avoiding NullPointerException
+        // Null checker for styles.css
         URL cssUrl = getClass().getResource("styles.css");
         if (cssUrl != null) {
             scene.getStylesheets().add(cssUrl.toExternalForm());
@@ -154,10 +236,21 @@ public class BookKeeper extends Application {
             System.err.println("Unable to find styles.css");
         }
 
+        // Set scene
         primaryStage.setScene(scene);
         primaryStage.show();
 
         // Dragging the window
+        windowControlsPane.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+
+        windowControlsPane.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX() - xOffset);
+            primaryStage.setY(event.getScreenY() - yOffset);
+        });
+
         titleBar.setOnMousePressed(event -> {
             xOffset = event.getSceneX();
             yOffset = event.getSceneY();
@@ -167,6 +260,27 @@ public class BookKeeper extends Application {
             primaryStage.setX(event.getScreenX() - xOffset);
             primaryStage.setY(event.getScreenY() - yOffset);
         });
+    }
+
+    private void toggleUserEditPane(StackPane userPane) {
+        VBox userEditPane = (VBox) userPane.getChildren().get(1);
+
+        if (!isUserEditPaneVisible) {
+            userEditPane.setVisible(true);
+
+            TranslateTransition slideIn = new TranslateTransition(Duration.millis(1000), userEditPane);
+            slideIn.setFromX(userEditPane.getWidth());
+            slideIn.setToX(0);
+            slideIn.play();
+            isUserEditPaneVisible = true;
+        } else {
+            TranslateTransition slideOut = new TranslateTransition(Duration.millis(1000), userEditPane);
+            slideOut.setFromX(0);
+            slideOut.setToX(userEditPane.getWidth());
+            slideOut.setOnFinished(e -> userEditPane.setVisible(false));
+            slideOut.play();
+            isUserEditPaneVisible = false;
+        }
     }
 
     public static void main(String[] args) {
